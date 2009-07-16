b5250450ccfd2a9cb55248a4f306b3c4c1208059
Kazuyoshi Tlacaelel <kazu.dev@gmail.com>
2009-07-16 08:21:23 +0000

    setup test suite

--- test/openid_store_active_record_test.rb
+++ test/openid_store_active_record_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class OpenidStoreActiveRecordTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+
   test "the truth" do
-    assert true
+    fail 'nothing implemented'
   end
+
 end
